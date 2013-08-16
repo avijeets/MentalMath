f0804e16b45b9eb4af8518d482095c84b017c45c
Avijeet Sachdev <info@avijeets.com>
2013-08-16 11:54:15 -0400

    added minus.py

---
+++ minus.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from random import randint
+
+print 'Correct responses receive 20 points, whereas wrong responses receive negative 10 points.  Type q to quit at any time.'
+score = 0
+while True:
+    a = randint(10, 200) #increase or decrease for difficulty of questions 
+    b = randint(10, 200) #increase or decrease for difficulty of questions
+    yourAnswer = raw_input('What is ' + str(a) + ' minus ' + str(b) + '?\n> ')
+    if yourAnswer == 'q':
+        break
+    elif yourAnswer == str(a - b):
+        score += 20
+        print 'Correct!  Total score: ' + str(score) + '\n'
+    else:
+        score -= 10
+        print 'Wrong! The correct answer was ' + str(a - b) + '. Total score: ' + str(score) + '\n'
+
+print 'Your score is ' + str(score) + '. Good luck next time!'

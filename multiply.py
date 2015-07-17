27ba8ab519c1e9b5a089427e3c0faf9a8cc13da3
Avijeet Sachdev <info@avijeets.com>
2015-07-16 22:38:11 -0400

    ranges are inputs now

--- multiply.py
+++ multiply.py
@@ -3,9 +3,12 @@ from random import randint
 
 print 'Correct responses receive 20 points, whereas wrong responses receive negative 10 points.  Type q to quit at any time.'
 score = 0
+start = input('Numbers should start from: ')
+end = input('Numbers should end at: ')
+
 while True:
-    a = randint(2, 100) #increase or decrease for difficulty of questions 
-    b = randint(2, 10) #increase or decrease for difficulty of questions
+    a = randint(start, end) #increase or decrease for difficulty of questions 
+    b = randint(start, end) #increase or decrease for difficulty of questions
     yourAnswer = raw_input('What is ' + str(a) + ' times ' + str(b) + '?\n> ')
     if yourAnswer == 'q':
         break
